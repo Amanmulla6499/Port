948a0343b9b0e1cf8aa76f9afaa9d5de14e827a3
Amanmulla6499 <94896013+Amanmulla6499@users.noreply.github.com>
2024-01-15 19:13:10 +0000

    Variables Complete Vid4

---
+++ .devcontainer/aman/variables.js
@@ -0,0 +1,29 @@
+const accounId =12345
+// const madhe apan je value lihto te change hot nai
+
+
+let accountEmail = "aman649@gmail.com"
+var accountPass = "12345"
+
+// bina variable che apn declare kru shakto 
+personCity = "Pune"
+
+// ya mdhe apna nusta variable declar kelo khr value nai dilo tr op la te undefinded manhun yete
+let accountState;
+
+accountEmail="mullaaman000@.com"
+accountPass="00000"
+personCity="Mumbai"
+
+// accountEmail="mulla.com"
+// accountPass="1111"
+// personCity="bengluru"
+
+// ya nusar pn kru shakto 
+console.log(accounId);
+
+// ya nusar pn kru shakto console.table use karun
+
+// he sagale ek line madhe lihta yete console.table ([ he square bracket takne , deun property change karne])
+
+console.table([accounId,accountEmail,accountPass,personCity,accountState])
