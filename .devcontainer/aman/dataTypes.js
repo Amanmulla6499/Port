79db461c1e804fcfbc2f0863201571d39f2703e5
Amanmulla6499 <94896013+Amanmulla6499@users.noreply.github.com>
2024-01-16 08:03:36 +0000

    Complete dataTypes

---
+++ .devcontainer/aman/dataTypes.js
@@ -0,0 +1,21 @@
+"use strict"; /*he ky saglya code la newer format nusar run karte */
+ let manAge = 18
+ let manName = "Abc"
+ let isLoggedIn=true
+ let manOp;
+
+console.log(3+3)
+// ani most karun semicolon la avoid karne
+// code readable asne most importanat ahe
+console.log("Aman")
+
+// kontya pn datatype che type olkhyache asle tr
+
+console.log(typeof "Aman"); // op= string
+console.log(typeof manAge); // op= Number
+console.log(typeof manName); // op= string
+console.log(typeof isLoggedIn);  // op=boolean
+console.log(typeof manOp); // op = undefined
+
+// null he ek object ahe
+console.log(typeof null);  // op= OBJECT
