549601b2b68d63c19a9e7da57c662d78b7dacf86
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-02-21 05:31:07 +0000

    NumMathinjavascript

---
+++ .devcontainer/aman/NumMath.js
@@ -0,0 +1,9 @@
+const Value = 100
+console.log(Value);
+
+const Num = new Number(400)
+console.log(Num);
+
+console.log(Num.toString().length);
+
+console.log(Num.toFixed(3)); // 400 chy nantar .000 vela yenar
