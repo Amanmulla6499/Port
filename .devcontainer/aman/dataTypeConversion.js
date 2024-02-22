c13f3848cc4c6297a88b16366fb4d44cdef17a65
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-02-22 07:30:49 +0000

    conversion

--- .devcontainer/aman/dataTypeConversion.js
+++ .devcontainer/aman/dataTypeConversion.js
@@ -8,7 +8,7 @@ let score = "33" // ya madhe true,none == Nan, undefined==NaN  he try karne
 
 // ya madhe pahila ek let varibale declare karne ya nanatar kasha madhe convert karayache ahe te datatype lihne Pahila word Capital mg konla conversion  karayche tycha name
 
-let value = Number(score)
+let value = Number(score)  // yache  conversion kartana = chy pude je yetay te phila word captial String,Number,Boolean,BigInt
 
 //console.log(value);
 
@@ -35,5 +35,18 @@ let anyNumber = 33
 let someNumber = String (anyNumber)
 
 console.log(someNumber);
-// he convert zalele astay string madhe pn value he number dakhwat astay
+// he convert zalele astay string madhe pn op value he number dakhwat astay
 console.log(typeof someNumber);
+
+// "33" = > 33
+// "33abc" = > Nan
+// true = 1 false = 0
+
+// Number to boolena conversion
+let ComeIn = 1  // ithe jr empty String Asel tr || "  " = String Empty  ==  false || "Aman" String Full == true
+let Out = Boolean(ComeIn)
+console.log(Out);
+
+let red = " " // op= False
+let blue = Boolean(red)
+console.log(blue);
