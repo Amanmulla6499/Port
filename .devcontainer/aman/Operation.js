3ac5f7ac57ecbc7414d0abda14e3bce40efebd59
Aman Mulla <94896013+Amanmulla6499@users.noreply.github.com>
2024-02-22 07:52:50 +0000

    Operation-in-js

---
+++ .devcontainer/aman/Operation.js
@@ -0,0 +1,32 @@
+let value = 3
+let negValue = -value
+// console.log(negValue); op = -3
+// + - * % /
+
+let Fname = "Aman"
+let Lname = " Mulla"
+
+let Fullname = Fname+Lname
+console.log(Fullname);
+
+
+// console.log("1"+2); // op == 12
+// console.log(1+"2"); // op == 12
+// console.log("1"+2+3); //op == 123  String pahila asle tr addition honar nai ahe ti 123 value print honar.
+// console.log(1+2+"3"); // op == 33 he kas ala string nantar asel tr pahila Addition honar mg ahe ti value yenar
+
+
+
+// PRE-FIX  || POST-FIX 
+
+let gameCounter = 100
+
+//gameCounter++ // he ahe post-fix =  last la increment hotay
+
+++gameCounter // he ahe pre-fix = pahila increment hotay
+
+console.log(gameCounter);
+
+//  op = 101
+
+ 
